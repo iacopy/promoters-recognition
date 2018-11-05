6d1740c688f6f6fe1b405ebae3de4cf637d1d517
Maziar <m.entezar@gmail.com>
2018-11-05 23:58:52 +0100

    keras model was not working. Hello worldified to basic working keras snippet for future improvements. Not a well code but now it trains.

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
-import tensorflow as tf
+import sys
+from keras.models import Sequential
+from keras.layers import Dense, Activation, Dropout
 
 import loader
 
-(x_train, y_train),(x_test, y_test) = loader.load_data()
+NUM_EPOCHS = 20
+TRAIN_BATCH_SIZE = 128
+TEST_BATCH_SIZE = 128
 
-model = tf.keras.models.Sequential([
-  tf.keras.layers.Flatten(),
-  tf.keras.layers.Dense(512, activation=tf.nn.relu),
-  tf.keras.layers.Dropout(0.2),
-  tf.keras.layers.Dense(10, activation=tf.nn.softmax)
-])
+(x_train, y_train),(x_test, y_test) = loader.load_data(sys.argv[1])
+
+model = Sequential()
+model.add(Dense(512, input_dim=500))
+model.add(Activation('relu'))
+model.add(Dropout(0.2))
+model.add(Dense(10))
+model.add(Activation('softmax'))
+
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, epochs=5)
-model.evaluate(x_test, y_test)
+model.fit(x_train, y_train, epochs=NUM_EPOCHS, batch_size=TRAIN_BATCH_SIZE)
+
+score = model.evaluate(x_test, y_test, batch_size=TEST_BATCH_SIZE)
+model.save('model.h5')
+print("Test -  loss: {}, accuracy: {}".format(score[0], score[1]))
